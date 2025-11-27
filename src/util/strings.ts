77b81e0de9ce78204e8e00c4055a0d208377ad56
Herik Vargas <herik02.11@gmail.com>
2025-11-26 20:12:48 -0500

    Updates

--- src/util/strings.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Capitalizes the first character of the given string.
- * Returns an empty string for empty input.
- */
-export const capitalize = (str?: string): string =>{
-  if(!str) return '';
-  return str.length === 0 ? '' : str.charAt(0).toUpperCase() + str.slice(1);
-}
